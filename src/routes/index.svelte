65fc10a6c31efd62c143e09cc90ebc19559c7126
Antoine-LB <antoine.stc25@gmail.com>
2022-07-12 16:44:37 +0200

    sort articles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,6 +30,18 @@
 <script lang="ts">
 	import Articulo from '../components/ArticuloComponent.svelte';
 	export let capitulos: RawCapitulo[];
+
+	function sortedArticulos(articulos: RawArticulo[]) {
+		return articulos.sort((a, b) => {
+			if (a.attributes.numero_de_articulo < b.attributes.numero_de_articulo) {
+				return -1;
+			}
+			if (a.attributes.numero_de_articulo > b.attributes.numero_de_articulo) {
+				return 1;
+			}
+			return 0;
+		});
+	}
 </script>
 
 <h1 class="text-4xl md:text-6xl pl-3 md:pl-6 font-extralight text-[#25f8b9db] my-5">
@@ -44,7 +56,7 @@
 	para hacerla mas entretenida.
 </p>
 
-<h1 class="text-4xl md:text-6xl pl-3 md:pl-6 font-extralight text-[#25f8b9db] my-5">
+<h1 class="text-3xl md:text-6xl pl-3 md:pl-6 font-extralight text-[#25f8b9db] my-5">
 	#SinArticuloNoHayDebate
 </h1>
 <p class="pl-3 md:pl-6 text-white text-lg">
@@ -107,7 +119,7 @@
 			</h3>
 		</div>
 		<div class="pl-3 md:pl-6 mb-6 md:mb-10">
-			{#each subCapitulo.attributes.articulos.data as articulo}
+			{#each sortedArticulos(subCapitulo.attributes.articulos.data) as articulo}
 				<Articulo {articulo} hex_color={subCapitulo.attributes.hex_color} />
 			{/each}
 		</div>
