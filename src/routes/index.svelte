82216e7040997bf58846557ec91ddad59cf911e7
Antoine-LB <antoine.stc25@gmail.com>
2022-07-12 06:00:16 +0200

    remouve article component to deploy to netlify

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,6 +29,11 @@
 
 <script lang="ts">
 	// import Articulo from '../components/Articulo.svelte';
+	import { getContext } from 'svelte';
+	import Popup from '../components/Popup.svelte';
+	const { open } = getContext('simple-modal');
+
+	const showSurprise = (articulo: RawArticulo) => open(Popup, { articulo: articulo });
 
 	export let capitulos: RawCapitulo[];
 </script>
@@ -41,8 +46,23 @@
 	<h2 class="text-white text-3xl">{capitulo.attributes.Nombre}</h2>
 	{#each capitulo.attributes.sub_capitulos.data as subCapitulo}
 		<h2 class="text-white text-xl pl-3">{subCapitulo.attributes.Nombre}</h2>
-		<!-- {#each subCapitulo.attributes.articulos.data as articulo}
-			<Articulo {articulo} hex_color={subCapitulo.attributes.hex_color} />
-		{/each} -->
+		{#each subCapitulo.attributes.articulos.data as articulo}
+			<button class="inline-block" on:click={() => showSurprise(articulo)}>
+				<div
+					class=" flex flex-col rounded-md p-2 w-[110px] h-[140px] articulo-animacion shadow-3xl m-2 cursor-pointer"
+					style={`background-color: #${subCapitulo.attributes.hex_color}`}
+				>
+					<div class="flex place-content-between">
+						<span
+							>{articulo.attributes.numero_de_articulo}({articulo.attributes
+								.numero_de_incisos})</span
+						>
+						<span>p.{articulo.attributes.pagina}</span>
+					</div>
+					<div class="m-auto text-6xl">{articulo.attributes.simbolo}</div>
+					<div class="m-auto text-xs">{articulo.attributes.nombre_corto}</div>
+				</div>
+			</button>
+		{/each}
 	{/each}
 {/each}
