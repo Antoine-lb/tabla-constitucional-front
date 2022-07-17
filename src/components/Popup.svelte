874de6ef9f7a3cee38c86cc06bb1ee802f2cca42
Antoine-LB <antoine.stc25@gmail.com>
2022-07-17 18:05:07 +0200

    target blanc and undo

--- src/components/Popup.svelte
+++ src/components/Popup.svelte
@@ -49,6 +49,7 @@
 <p class="font-bold text-md">Página: {articulo.attributes.pagina}</p>
 <a
 	class="underline"
+	target="blanc"
 	href={`https://www.chileconvencion.cl/wp-content/uploads/2022/07/Texto-CPR-2022.pdf#page=${
 		articulo.attributes.pagina + 4
 	}`}>Abrir página {articulo.attributes.pagina} en el PDF oficial</a
@@ -78,10 +79,10 @@
 		Artículo #{articulo.attributes.numero_de_articulo}
 	</a>
 
-	<button
+	<!-- <button
 		type="button"
 		class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
 	>
 		Copiar Artículo #{articulo.attributes.numero_de_articulo}
-	</button>
+	</button> -->
 </div>
