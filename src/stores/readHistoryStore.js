e5d67c8a5355d8b1c81a334aa8f2dc46374859db
Antoine-LB <antoine.stc25@gmail.com>
2022-07-17 14:03:55 +0200

    read hsitory localhost gone

--- src/stores/readHistoryStore.js
+++ src/stores/readHistoryStore.js
@@ -1,3 +1,5 @@
-import { localStore } from './localStorageStore';
+// import { localStore } from './localStorageStore';
+// export const readHistory = localStore('history', []);
 
-export const readHistory = localStore('history', []);
+import { writable } from 'svelte/store';
+export const readHistory = writable([]);
